831aa577a5e0102121fda6be9130eaf60b15882b
hillary1343 <lindsayhillary8@gmail.com>
2024-09-26 18:35:17 -0300

    versão inicial

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Noite. Um silvo no ar");
+Console.WriteLine("Ninguém na estação. E o trem");
+Console.WriteLine("passa sem parar.");
